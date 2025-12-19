b1ed9187fe56b5463878bcf7769fd6aa95d4931e
araja2458 <araja2458@gmail.com>
2025-12-20 00:01:20 +0500

    Update menu.tsx

--- frontend/src/pages/menu.tsx
+++ frontend/src/pages/menu.tsx
@@ -215,7 +215,7 @@ export default function MenuPage() {
         item={selectedItem ?? undefined}
       />
 
-      {/* WHATSAPP */}cd 
+      {/* WHATSAPP */} 
       <a
         href="https://wa.me/61460316046"
         target="_blank"
