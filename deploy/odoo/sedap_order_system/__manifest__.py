9fb0ffcc9d576a10750eb30d60cb3c39f7508390
Ayesha Raja <araja2458@gmail.com>
2026-08-26 22:41:44 +0500

    Revert "Admin switch to hide a product from the website only"
    This reverts commit c73b15d272b6025e9123b378bce6849b9ad06472.

--- deploy/odoo/sedap_order_system/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-   'name': 'Sedap Order System',
-   'version': '1.1',
-   'depends': ['base', 'sale', 'point_of_sale'],
-   'data': [
-       'views/product_online_views.xml',
-   ],
-   'installable': True,
-}
